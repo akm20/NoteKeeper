31bfc880275bfa92c1cbc4a7fd80a1601e95c159
akm20 <106100557+akm20@users.noreply.github.com>
2022-07-08 21:52:54 +0530

    Add files via upload

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,1 +1,21 @@
-a
+const express = require("express");
+const {
+  getNotes,
+  createNote,
+  getNoteById,
+  UpdateNote,
+  DeleteNote,
+} = require("../controllers/noteController");
+const { protect } = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+router.route("/").get(protect, getNotes);
+router.route("/create").post(protect, createNote);
+router
+  .route("/:id")
+  .get(protect, getNoteById)
+  .put(protect, UpdateNote)
+  .delete(protect, DeleteNote);
+
+module.exports = router;
