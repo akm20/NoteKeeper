1e3f2fdbb56810f2a5ef48b660cbbf3b2489e2c7
akm20 <106100557+akm20@users.noreply.github.com>
2022-07-09 01:01:53 +0530

    Add files via upload

---
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const userInfo = localStorage.getItem("userInfo");
+    if (userInfo) {
+      navigate("/mynotes");
+    }
+  }, [navigate]);
+
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div>
+            <div className="intro-text">
+              <h1 className="title">Welcome to NoteKeeper</h1>
+              <p className="subtitle">One safe place for all your notes</p>
+            </div>
+            <div className="buttonContainer">
+              <a href="/login">
+                <Button size="lg" className="landingbutton">
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button
+                  size="lg"
+                  className="landingbutton"
+                  variant="outline-primary"
+                >
+                  Sign Up
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default LandingPage;
