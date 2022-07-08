142c7d400538b96584b2c45240de4ff28776cc1b
akm20 <106100557+akm20@users.noreply.github.com>
2022-07-08 21:37:12 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
+const app = express();
+dotenv.config();
+connectDB();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes);
+// });
+
+app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+// app.get("/api/notes/:id", (req, res) => {
+//   const note = notes.find((n) => n._id === req.params.id);
+//   res.send(note);
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server has started on ${PORT}`));
